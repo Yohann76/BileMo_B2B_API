2594a8af4ac62f4443f86004f73adbed6bc13909
Yohann76 <yohanndurand76@gmail.com>
2019-11-11 16:55:17 +0100

    search ressource and add resource

---
+++ src/Controller/PhoneController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Phone;
+use App\Repository\PhoneRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * @Route("/api")
+ */
+class PhoneController extends AbstractController
+{
+    /**
+     * @Route("/phones/{id}", name="show_phone", methods={"GET"})
+     */
+    public function show(Phone $phone, PhoneRepository $phoneRepository, SerializerInterface $serializer)
+    {
+        $phone = $phoneRepository->find($phone->getId());
+        $data = $serializer->serialize($phone, 'json', [
+            'groups' => ['show']
+        ]);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/phones/{page<\d+>?1}", name="list_phone", methods={"GET"})
+     */
+    public function index(Request $request, PhoneRepository $phoneRepository, SerializerInterface $serializer)
+    {
+        $page = $request->query->get('page');
+        if(is_null($page) || $page < 1) {
+            $page = 1;
+        }
+        $phones = $phoneRepository->findAllPhones($page, 10 ); // todo : getenv('LIMIT')
+        $data = $serializer->serialize($phones, 'json', [
+            'groups' => ['list']
+        ]);
+        return new Response($data, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @Route("/phones", name="add_phone", methods={"POST"})
+     */
+    public function new(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, ValidatorInterface $validator)
+    {
+        $phone = $serializer->deserialize($request->getContent(), Phone::class, 'json');
+        $errors = $validator->validate($phone);
+        if(count($errors)) {
+            $errors = $serializer->serialize($errors, 'json');
+            return new Response($errors, 500, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        $entityManager->persist($phone);
+        $entityManager->flush();
+        $data = [
+            'status' => 201,
+            'message' => 'Le téléphone a bien été ajouté'
+        ];
+        return new JsonResponse($data, 201);
+    }
+
+}
---
+++ src/Entity/Phone.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"list", "show"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"list", "show"})
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     * @Assert\Length(min="2", minMessage="Ce champ doit contenir un minimum de {{ limit }} caractères", max="255", maxMessage="Ce champ doit contenir un maximum de {{ limit }} caractères")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Groups({"list", "show"})
+     * @Assert\NotBlank(message="Ce champ ne doit pas être vide")
+     * @Assert\Range(min="0", minMessage="La valeur minimum autorisée est {{ limit }}", max="1500", maxMessage="La valeur maximum autorisée est {{ limit }}")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"show"})
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Groups({"show"})
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
